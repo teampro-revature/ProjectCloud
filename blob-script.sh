dd7bcc24dc0f16027b7d8fbebac746ac1e66b2d0
notsew710 <49280719+notsew710@users.noreply.github.com>
2019-05-15 21:25:28 -0500

    added in restrictions

--- blob-script.sh
+++ blob-script.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
+createStorages()
+{
 resourcegroup=$1
 blobStorageAccount=$2
-
-az group create -n $resourcegroup -l southcentralus
-
-az storage account create --name $blobStorageAccount \
---location southcentralus --resource-group $resourcegroup \
---sku Standard_LRS --kind blobstorage --access-tier hot
+createdStorage=$(az storage account list --query [].name | grep -E $blobStorageAccount)
+if [ -z $created storage ]; then 
+  az storage account create \
+  --name $blobStorageAccount \
+--location southcentralus \
+--resource-group $resourcegroup \
+--sku Standard_LRS \
+--kind blobstorage \
+--access-tier hot
 
 blobStorageAccountKey=$(az storage account keys list -g $resourcegroup \
 -n $blobStorageAccount --query [0].value --output tsv)
+}
 
-az storage container create -n images --account-name $blobStorageAccount \
+createContainers ()
+{
+blobStorageAccount=$1
+containerName=$2
+blobStorageAccountKey=$(az storage account keys list -g $resourcegroup \
+-n $blobStorageAccount --query [0].value --output tsv)
+createdContainer=$(az storage container list --account-name $blobStorageAccount --query [].name | grep -E $containerName)
+if [ -createdContainer ]; then 
+az storage container create -n $containerName --account-name $blobStorageAccount \
 --account-key $blobStorageAccountKey --public-access container 
+}
