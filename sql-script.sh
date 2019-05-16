90ecd98566d66be6a49d5ec5db55ef2e3289a9cd
notsew710 <49280719+notsew710@users.noreply.github.com>
2019-05-15 21:41:01 -0500

    Update sql-script.sh

--- sql-script.sh
+++ sql-script.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
+createSQL()
+{
 # Set variables for the new SQL
 resourceGroup=$1
 myserver=$2 
 mydb=$3
 
-
-
-# Create a resource group
-az group create \
-    --name $resourceGroup \
-    --location southcentralus
-
-
 #Create a Standard S0 database.
+createdSQL=$(az sql db list --$myserver --resourceGroup --query [].name | grep -E $mydb)
+if [ -z $createdSQL ]; then
 az sql db create \
       --resource-group $resourceGroup --server $myserver --name $mydb --service-objective S0
+
+}
 
 
 az sql db delete \
