bd10ccd55aefc8172def94e7fc6d82ed5dd2c358
notsew710 <49280719+notsew710@users.noreply.github.com>
2019-05-17 10:56:25 -0500

    removed delete function

--- rsg.sh
+++ rsg.sh
@@ -1,7 +1,5 @@
-!/bin/bash
+#!/bin/bash
 
-createGroup() 
-{
     groupname=$1
     locations=$2
 
@@ -18,20 +16,4 @@ createGroup()
         echo "error"
         exit 1
     fi
-}
 
-deleteGroup()
-{
-        createdGroup=$(az group list --query [].name | grep -E $groupname)
-    if ! [ -z $createdGroup ]; then
-            az group delete --resource-group $groupname 
-
-            echo "deleted"
-    else
-        echo "error"
-        exit 1
-    fi
-}
-
-command $1
-$2 $3
