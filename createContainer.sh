e0b784761118f83ffdd7e98848d5aacd5524bf1e
notsew710 <49280719+notsew710@users.noreply.github.com>
2019-05-17 14:03:19 -0500

    Update createContainer.sh

--- createContainer.sh
+++ createContainer.sh
@@ -8,8 +8,11 @@ groupname=$3
 blobStorageAccountKey=$(az storage account keys list -g $groupname \
 -n $blobStorageAccount --query [0].value --output tsv)
 
-createdContainer=$(az storage container list --account-name $blobStorageAccount --query [].name | grep -E $container$if [ -createdContainer ]; then
-az storage container create -n $containerName --account-name $blobStorageAccount \
- --account-key $blobStorageAccountKey --public-access container
+createdContainer=$(az storage container list --account-name $blobStorageAccount --query [].name | grep -E $containername)
+if [ -z createdContainer ]; then
+az storage container create -n $containerName \
+--account-name $blobStorageAccount \
+ --account-key $blobStorageAccountKey \
+ --public-access container
 
 f
