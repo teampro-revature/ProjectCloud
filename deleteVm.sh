42c670a5947ca86752f425c4df015c7e196e353f
notsew710 <49280719+notsew710@users.noreply.github.com>
2019-05-17 11:23:59 -0500

    Update deleteVm.sh

--- deleteVm.sh
+++ deleteVm.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-    vmName=$2
-    groupName=$3
+   vmName=$1
+   groupname=$2
 
-    createdVm=$(az vm list --query [].name | grep -E $vmName)
-    if 
-        [
-            -z $createdVm   
-        ]; then 
-            az vm delete --name $VMname \
-            --resource-group $groupname 
+    createdVm=$(az group list --query [].name | grep -E $vmName)
+    if [ -z $createdVm ]; then
+            az vm delete -n $vmName -g $groupname
 
             echo "deleted"
     else
+        echo "error"
         exit 1
     fi
