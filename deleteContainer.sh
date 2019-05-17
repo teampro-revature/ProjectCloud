b130f1597169fe192225e8654470e2614f771827
notsew710 <49280719+notsew710@users.noreply.github.com>
2019-05-17 13:49:10 -0500

    updated

--- deleteContainer.sh
+++ deleteContainer.sh
@@ -1,10 +1,10 @@
-Missing credentials to access storage service. The following variations are accepted:
-    (1) account name and key (--account-name and --account-key options or
-        set AZURE_STORAGE_ACCOUNT and AZURE_STORAGE_KEY environment variables)
-    (2) account name and SAS token (--sas-token option used with either the --account-name
-        option or AZURE_STORAGE_ACCOUNT environment variable)
-    (3) account name (--account-name option or AZURE_STORAGE_ACCOUNT environment variable;
-        this will make calls to query for a storage account key using login credentials)
-    (4) connection string (--connection-string option or
-        set AZURE_STORAGE_CONNECTION_STRING environment variable); some shells will require
-        quoting to preserve literal character interpretation.
+#!/bin/bash
+
+groupname=$1
+blobStorageAccount=$2
+containername=$3
+
+blobStorageAccountKey=$(az storage account keys list -g $groupname \
+-n $blobStorageAccount --query [0].value --output tsv)
+
+az storage container delete --account-name $blobStorageAccount --account-key $blobStorageAccountKey -n $containernam$
