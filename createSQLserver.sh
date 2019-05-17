ffc6190f8d9d44d9ec66c613e61545b7c5922118
notsew710 <49280719+notsew710@users.noreply.github.com>
2019-05-17 11:51:18 -0500

    Update and rename sql-script.sh to createSQLserver.sh

--- createSQLserver.sh
+++ createSQLserver.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-createSQL()
-{
-# Set variables for the new SQL
-resourceGroup=$1
-myserver=$2 
-mydb=$3
+groupname=$1
+myserver=$2
+admin=$3
+adminpass=$4
 
 #Create a Standard S0 database.
-createdSQL=$(az sql db list --$myserver --resourceGroup --query [].name | grep -E $mydb)
-if [ -z $createdSQL ]; then
+az sql server create -l southcentralus -g $groupname -n $myserver -u $admin -p $adminpass
 az sql db create \
       --resource-group $resourceGroup --server $myserver --name $mydb --service-objective S0
 
