5b58c8674ed78f04719cc942524408d7d855718e
notsew710 <49280719+notsew710@users.noreply.github.com>
2019-05-17 11:59:55 -0500

    Update createSQLserver.sh

--- createSQLserver.sh
+++ createSQLserver.sh
@@ -7,10 +7,3 @@ adminpass=$4
 
 #Create a Standard S0 database.
 az sql server create -l southcentralus -g $groupname -n $myserver -u $admin -p $adminpass
-
-
-az sql db delete \
-      --resource-group $resourceGroup --server $myserver --name $mydb --service-objective S0
-
-
-      
