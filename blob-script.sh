6389cf7a027e2207cf02799719fffd48c5094915
notsew710 <49280719+notsew710@users.noreply.github.com>
2019-05-17 13:30:28 -0500

    Update blob-script.sh

--- blob-script.sh
+++ blob-script.sh
@@ -1,31 +1,20 @@
 #!/bin/bash
 
-createStorages()
-{
 resourcegroup=$1
 blobStorageAccount=$2
+
 createdStorage=$(az storage account list --query [].name | grep -E $blobStorageAccount)
-if [ -z $created storage ]; then 
+if [ -z $createdStorage ]; then
   az storage account create \
-  --name $blobStorageAccount \
+  -n $blobStorageAccount \
 --location southcentralus \
---resource-group $resourcegroup \
+-g $resourcegroup \
 --sku Standard_LRS \
 --kind blobstorage \
 --access-tier hot
 
-blobStorageAccountKey=$(az storage account keys list -g $resourcegroup \
--n $blobStorageAccount --query [0].value --output tsv)
-}
+fi
 
-createContainers ()
-{
-blobStorageAccount=$1
-containerName=$2
 blobStorageAccountKey=$(az storage account keys list -g $resourcegroup \
 -n $blobStorageAccount --query [0].value --output tsv)
-createdContainer=$(az storage container list --account-name $blobStorageAccount --query [].name | grep -E $containerName)
-if [ -createdContainer ]; then 
-az storage container create -n $containerName --account-name $blobStorageAccount \
---account-key $blobStorageAccountKey --public-access container 
-}
+echo "$blobStorageAccount"
