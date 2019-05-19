45dec0eedf94c51c43809e5d39b4e5cf771e8e9e
notsew710 <49280719+notsew710@users.noreply.github.com>
2019-05-18 20:54:19 -0500

    Update server.js

--- LoginAuth/server.js
+++ LoginAuth/server.js
@@ -4,7 +4,7 @@ var morgan = require('morgan');
 var path = require('path');
 
 // Initialize variables.
-var port = 30662; // process.env.PORT || 30662;
+var port = 8080; // process.env.PORT || 8080;
 
 // Configure morgan module to log all requests.
 app.use(morgan('dev'));
