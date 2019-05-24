e14f0719efc796326baa08442892e2ae8d551b53
notsew710 <49280719+notsew710@users.noreply.github.com>
2019-05-24 10:19:47 -0500

    Update DeleteNt.sh

--- DeleteNt.sh
+++ DeleteNt.sh
@@ -3,3 +3,12 @@
 net=$1
 group=$2
 
+       createdNet=$(az network vnet list --query [].name | grep -E $net)
+    if ! [ -z $createdNet ]; then
+            az network vnet delete --resource-group $groupname --name $net 
+
+            echo "deleted"
+    else
+        echo "error"
+        exit 1
+    fi
