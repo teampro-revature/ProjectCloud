43ceaab463b01af536fc1d00738a4047e536f44d
notsew710 <49280719+notsew710@users.noreply.github.com>
2019-05-27 15:20:22 -0500

    Update server.js

--- login/server.js
+++ login/server.js
@@ -1,8 +1,3 @@
-/*
-*  Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license.
-*  See LICENSE in the source repository root for complete license information.
-*/
-
 var express = require('express');
 var app = express();
 var morgan = require('morgan');
