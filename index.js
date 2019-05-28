139a5f33005c492680284244467db757acaaa471
joshbrahim <49167258+joshbrahim@users.noreply.github.com>
2019-05-27 19:47:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 app.get('/',function(req,res) {
-  res.sendFile(path.join(__dirname + 'index.html'));
+  res.sendFile(path.join(__dirname + '/view + '/index.html'));
 });
 
 app.get('/main',function(req,res) {
